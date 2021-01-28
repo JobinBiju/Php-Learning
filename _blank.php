68c5960d3ab0ba589ee15c202810b923f30a66e0
JobinBiju <jobinbiju9090@gmail.com>
2021-01-28 09:11:38 +0530

    first commit

---
+++ _blank.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$name = "Jobin";
+echo $name . " " . $name;
+
+?>
+</body>
+</html>
---
+++ _practical-app/1.php
@@ -0,0 +1,29 @@
+<?php include "functions.php" ?>
+<?php include "includes/header.php" ?>
+
+	<section class="content">
+
+	<aside class="col-xs-4">
+
+	<?php Navigation();?>
+			
+			
+	</aside><!--SIDEBAR-->
+
+
+<article class="main-content col-xs-8">
+		
+
+
+		<?php  
+
+		// prints hello world in H1
+		echo "<h1>Hello Bro</h1>";
+
+		?>
+
+	
+
+		</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php" ?>
