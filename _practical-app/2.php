72eaed5e30a47898ff96e7abb6d69e8fff697b45
JobinBiju <jobinbiju9090@gmail.com>
2021-01-30 22:25:46 +0530

    Practice section 2

--- _practical-app/2.php
+++ _practical-app/2.php
@@ -17,22 +17,16 @@
 
 		<?php
 
-		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+		$number1 = 10;
+		$number2 = 20;
 
-		  Step 2: Add the two variables and display the sum with echo:
+		echo $number1 + $number2 . "<br>";
 
-
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
-
-
-			
-
-			 */
-
+		$list = array(34, 45, 56);
+		$list_ass = array("PHP" => 70);
 		
-
+		echo $list[2] . "<br>";
+		echo $list_ass['PHP'];
 
 		?>
 
